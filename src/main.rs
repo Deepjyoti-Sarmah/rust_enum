a65e7b4498d50196bee456de25a18ee7b14294ca
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-06-08 12:05:48 +0530

    method enums

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 enum IpAddrKind{
-    v4,
-    v6,
+    v4{u8, u8, u8, u8},
+    v6{String},
+}
+
+enum Message{
+    Ouit,
+    Move {x: i32, y:i32},
+    Write{String},
+    ChangeColor{i32, i32, i32},
+}
+
+impl  Message{
+    fn some_function() {
+        println!("Lets Get Rusty");
+    }
 }
 
 struct IpAddr{
@@ -9,13 +22,15 @@ struct IpAddr{
 }
 
 fn main() {
-    let four : IpAddrKind = IpAddrKind::v4;
-    let six : IpAddrKind = IpAddrKind::v6;
+    // let four : IpAddrKind = IpAddrKind::v4;
+    // let six : IpAddrKind = IpAddrKind::v6;
 
-    let localhost: IpAddr = IpAddr{
-        kind: IpAddrKind::v4,
-        address: String::from("127.0.0.1")
-    }
+    // let localhost: IpAddr = IpAddr{
+    //     kind: IpAddrKind::v4,
+    //     address: String::from("127.0.0.1")
+    // }
+
+    let localhost: IpAddrKind = IpAddrKind::v4(String::from(127, 0,0, 1));
 
 }
 
