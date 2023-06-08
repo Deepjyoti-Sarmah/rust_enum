9e4927cc085adc0b1bfa5e1f9875dec64bd452de
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-06-08 11:59:33 +0530

    adding struct with enums

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    
+enum IpAddrKind{
+    v4,
+    v6,
 }
+
+struct IpAddr{
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+    let four : IpAddrKind = IpAddrKind::v4;
+    let six : IpAddrKind = IpAddrKind::v6;
+
+    let localhost: IpAddr = IpAddr{
+        kind: IpAddrKind::v4,
+        address: String::from("127.0.0.1")
+    }
+
+}
+
+fn route(ip_kind: IpAddrKind) {}
